b36f3ac16b407ea1e2cac322a3d30eb36eb92fd5
Jamilly <ronijamilly.lima@sptech.school>
2025-11-19 23:29:26 -0300

    configurando arquivos para integrar API no cadastro

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,34 @@
+-- Scripts ConectUs Projeto Individual 2025_2
+create database Conectus;
+
+use Conectus;
+
+create table Usuario(
+idUsuario int primary key auto_increment,
+nome varchar(45) not null,
+dtNasc date not null,
+email varchar(45) not null unique,
+tipoEscola enum('publica', 'privada') not null,
+conhecimento enum('Sim', 'Nao') not null,
+detalhamento varchar(45) ,
+senha varchar(45) not null
+);
+
+create table Postagem(
+idPostagem int primary key auto_increment,
+assunto varchar(45),
+comentario varchar(250),
+fkUsuario int,
+constraint fkUsuarioPostagem
+	foreign key (fkUsuario)
+    references Usuario(idUsuario)
+);
+
+create table Resposta(
+idResposta int primary key auto_increment,
+descricao varchar(250),
+fkPostagem int,
+constraint fkPostagemResposta
+	foreign key (fkPostagem)
+	references Postagem(idPostagem)
+);
